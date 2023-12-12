256a02eeef10c1bc6bbd0a88afd00a1a538fe7ca
Erlan Suherlan <erlan.suherlan@cordlife.com>
2023-12-12 13:34:21 +0700

    jwt, register, login

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class ApiController extends Controller
+{
+    //REGISTER USER
+    public function register(Request $request) {
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
+
+        // User Model
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+
+        // Response
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
+    }
+
+    public function login(Request $request) {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        //JWT AUTH
+        $token = JWTAuth::attempt([
+            "email" => $request->email,
+            "password" => $request->password
+        ]);
+
+        if (!empty($token)) {
+            return response()->json([
+                "status" => true,
+                "message" => "User logged in successfully",
+                "token" => $token
+            ]);
+        }
+
+        return response()->json([
+            "status" => false,
+            "message" => "Invalid"
+        ]);
+    }
+}
